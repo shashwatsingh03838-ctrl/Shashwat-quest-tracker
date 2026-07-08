50e3e704e7fda50bd6adf6ef0af6dfc0e6a3c596
shashwatsingh03838-ctrl <shashwatsingh03838@gmail.com>
2026-07-08 16:40:34 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "quest-log-v1";
+const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
